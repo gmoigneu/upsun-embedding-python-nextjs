d1f0a9d7b7379460fe4126eda67d5ca391ca2ac9
Fabien Potencier <fabien@potencier.org>
2024-06-19 11:11:58 +0200

    -

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,16 +1,12 @@
 "use server"
-import prisma from '@/lib/prisma';
-import { extract, toMarkdown } from '@/lib/format';
-import { HfInference } from '@huggingface/inference';
-import { createStreamableValue } from 'ai/rsc';
+import prisma from '@/lib/prisma'
+import { extract, toMarkdown } from '@/lib/format'
+import { Ollama } from 'ollama'
+import { createStreamableValue } from 'ai/rsc'
 
 export async function recommend(messages: Message[]) {
-  
-  const inference = new HfInference(process.env.HUGGINGFACE_TOKEN);
-  const embedding = await inference.featureExtraction({
-    model: "sentence-transformers/all-MiniLM-L6-v2",
-    inputs: messages[messages.length - 1].content,
-  });
+  const ollama = new Ollama({ host: (process.env.OLLAMA_SCHEME || 'http')+'://'+(process.env.OLLAMA_HOST || 'localhost')+':'+(process.env.OLLAMA_PORT || 11434) })
+  const embedding = await ollama.embeddings({ model: process.env.EMBEDDING_MODEL || 'not-defined', prompt: messages[messages.length - 1].content })
 
   // Query the database for similar watches
   const watches: Watch[] = await prisma.$queryRaw`
@@ -29,34 +25,42 @@ export async function recommend(messages: Message[]) {
       complications,
       power_reserve,
       price_usd,
-      1 - (embedding <=> ${embedding}::vector) as similarity
+      1 - (embedding <=> ${embedding['embedding']}::vector) as similarity
     FROM watches
     ORDER BY  similarity DESC
     LIMIT 5;
     `
-  
-  const stream = createStreamableValue('');
-  (async () => {
 
+  const stream = createStreamableValue('')
+  const message = {
+    'role': 'user',
+    'content': `
+You are an helpful AI that will recommend watches to the user.
+
+The watch selected is below with its attributes as a list.
+Your response is a markdown document.
+Write a paragraph of why this watch matches their request using the watch attributes.
+
+The recommended watch is:
+
+${toMarkdown(watches[0])}.
+
+Create a table of the following data titled as alternatives:
+
+${watches.map((watch: Watch) => extract(watch)).join('\n')}
+`,
+  };
+  (async () => {
     // Update the stream with all the watches[0] properties in a markdown table
     stream.append(toMarkdown(watches[0]))
     stream.append(`\n\n\n\n`)
 
-    for await (const chunk of inference.chatCompletionStream({
-      model: "mistralai/Mistral-7B-Instruct-v0.2",
-      messages: [
-        {
-          'role': 'user',
-          'content': `You are an helpful AI that will recommend watches to the user. The watch selected is below with its attributes as a list. Your response is a markdown document. Write a paragrah of why this watch matches their request using the watch attributes. The recommended watch is: ${toMarkdown(watches[0])}. Create a table of the following data titled as alternatives: ${watches.map((watch: Watch) => extract(watch)).join('\n')}`
-        }
-      ],
-      max_tokens: 500,
-      temperature: 0,
-    })) {
-      stream.update(chunk.choices[0].delta.content!);
+    const response = await ollama.chat({ model: process.env.CHAT_MODEL || 'not-defined', messages: [message], stream: true })
+    for await (const part of response) {
+      stream.update(part.message.content)
     }
-    stream.done();
-  })();
+    stream.done()
+  })()
 
-  return { output: stream.value };
+  return { output: stream.value }
 }
